0878cb23f87249f8fd5083a019b1680581534cef
Denisa Urluescu <8223441+nyklt@users.noreply.github.com>
2022-05-18 10:58:14 +0300

    Checkout template: Render/Remove/Clear items from local storage

--- seed.js
+++ seed.js
@@ -1,8 +1,9 @@
-funtion seed() {
+function seed() {
 	localStorage.setItem("cart", "");
 	localStorage.setItem("feature-flags", "");
 	localStorage.setItem("products_ember", "");
 	localStorage.setItem("products_react", "");
+	localStorage.setItem("items", '["Item 1","Item 2","Item 3","Item 4"]');
 
 	// add seeds here
 }
